8eab385e9406c54fee8f004c692f00c03ff4cc5e
hossam <hossam7009@gmail.com>
2026-06-16 08:24:09 +0000

    add http ingress for security group

--- infra/main.tf
+++ infra/main.tf
@@ -69,6 +69,14 @@ resource "aws_security_group" "web_sg" {
   name        = "allow_web_traffic"
   description = "Allow SSH and HTTP traffic"
   vpc_id      = aws_vpc.main.id
+  
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
 
   ingress {
     description = "HTTPS from anywhere"
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,3 +1,3 @@
 aws_region = "eu-west-1"
 vpc_cidr   = "10.0.0.0/16"
-vpc_name   = "production-vpc"
+vpc_name   = "Hossam-production-vpc"
